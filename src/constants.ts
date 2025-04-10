66eb9fe52fbd7308f59b2d3e66e14777da124951
smals-jy <143497095+smals-jy@users.noreply.github.com>
2025-04-10 13:02:45 +0200

    feat: dynamic date & time

--- src/constants.ts
+++ src/constants.ts
@@ -24,8 +24,8 @@ export const PREDEFINED_FIELDS = {
   PATIENT_SEX: "male",
   PATIENT_BIRTHDAY: "1992-06-19",
   // Date info
-  DATE: "2018-04-19",
-  TIME: "14:30:09",
+  DATE: getFormattedDate(),
+  TIME: getFormattedTime(),
   // MS version
   MS_VERSION: "42",
 };
@@ -36,6 +36,27 @@ export function addPrefix(prefix: string, element: string) {
     return element;
   }
   return `${prefix}:${element}`;
+}
+
+// Generate a date
+function getFormattedDate(offsetDays = 1) {
+  const date = new Date();
+  date.setDate(date.getDate() - offsetDays);
+
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0'); // Mois commence à 0
+  const day = String(date.getDate()).padStart(2, '0');
+
+  return `${year}-${month}-${day}`;
+}
+
+// Generate the current time, as formatted
+function getFormattedTime(date = new Date()) {
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
+
+  return `${hours}:${minutes}:${seconds}`;
 }
 
 // Generate a random ssin
