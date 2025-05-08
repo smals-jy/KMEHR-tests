eac36cd0f44eec854d439daa423b86d04f1aff10
smals-jy <143497095+smals-jy@users.noreply.github.com>
2025-05-08 15:00:21 +0200

    Create recipePP-1.28-example20.ts

---
+++ configurations/fhir-prescriptions/recipePP-1.28-example20.ts
@@ -0,0 +1,40 @@
+import type { FHIRPrescriptionConfiguration } from "@smals-jy/kmehr-tests";
+
+export default function (): FHIRPrescriptionConfiguration {
+    return {
+        type: "P0",
+        transactions: [
+            {
+                id: 1,
+                drug: {
+                    drugType: "substanceproduct",
+                    identifierType: "CD-VMPGROUP",
+                    intendedcd: "0000174",
+                    intendedname: "aceclofenac oraal 100 mg",
+                    periodicity: "D",
+                    duration: {
+                        quantity: 10,
+                        timeunit: "d"
+                    },
+                    route: "00060",
+                    regimen: [
+                        {
+                            quantity: 1,
+                            unit: "00005",
+                            timeOfDay: {
+                                dayPeriod: "duringbreakfast"
+                            }
+                        },
+                        {
+                            quantity: 1,
+                            unit: "00005",
+                            timeOfDay: {
+                                dayPeriod: "duringdinner"
+                            }
+                        }
+                    ],
+                }
+            }
+        ]
+    }
+}
