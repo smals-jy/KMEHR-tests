5c81e76959375331c83e466a11f5f32e9cf594d3
smals-jy <143497095+smals-jy@users.noreply.github.com>
2025-08-07 13:15:11 +0200

    Update SideBySideTreeCompare.tsx

--- documentation/src/components/SideBySideTreeCompare/SideBySideTreeCompare.tsx
+++ documentation/src/components/SideBySideTreeCompare/SideBySideTreeCompare.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import ReactJsonView from '@microlink/react-json-view'
 import ReactXmlViewer from 'react-xml-viewer';
 
