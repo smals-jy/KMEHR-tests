0041cd5c1de63a0099d447b3b501a14a518d4cb0
smals-jy <143497095+smals-jy@users.noreply.github.com>
2025-08-08 08:47:42 +0200

    Update index.tsx

--- documentation/src/components/SideBySideTreeCompare/index.tsx
+++ documentation/src/components/SideBySideTreeCompare/index.tsx
@@ -1,2 +1,2 @@
-export { SideBySideTreeCompare } from './SideBySideTreeCompare';
+export { default as SideBySideTreeCompare } from './SideBySideTreeCompare';
 export type { Props as SideBySideTreeCompareProps } from './SideBySideTreeCompare';
