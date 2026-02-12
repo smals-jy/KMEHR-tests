2bddd4293fb2307d94ac2853946b1efe207449be
smals-jy <143497095+smals-jy@users.noreply.github.com>
2026-02-12 11:09:37 +0100

    Add TS-15-medicationuse configuration file

---
+++ configurations/ms/TS-15-medicationuse.ts
@@ -0,0 +1,29 @@
+import type { MSConfiguration } from "@smals-jy/kmehr-tests";
+
+export default function (): MSConfiguration {
+  
+  // To generate a number to a minimal length
+  function numberToPaddedString(num: number, minLength: number = 3): string {
+    const str = num.toString();
+    const paddingLength = Math.max(0, minLength - str.length);
+    const padding = "0".repeat(paddingLength);
+    return padding + str;
+  }
+
+  const medicationUses = [
+    undefined,
+    "For stomachache"
+  ]
+
+  return {
+    transactions: medicationUses.map((r, idx) => ({
+      id: idx + 2,
+      drug: {
+        drugType: "compoundprescription",
+        compoundprescriptionText: `${numberToPaddedString(idx + 1)}`,
+        periodicity: "D",
+        posologyFreeText: `${ (r !== undefined) ? "With medicationuse": "Without medicationuse" }`
+      },
+    })),
+  };
+}
