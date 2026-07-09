caf98cee5da0c491429f7ad5e53cb69a3016d7fe
smals-jy <143497095+smals-jy@users.noreply.github.com>
2026-07-09 13:30:13 +0200

    Add corner cases configuration for PCDH
    This file defines a PCDHConfiguration with patient and delivery details, including corner cases for medication quantities.

---
+++ configurations/pcdh/TS-05-corner-cases.ts
@@ -0,0 +1,47 @@
+import type { PCDHConfiguration } from "../../src/config";
+
+export default function (): PCDHConfiguration {
+  return {
+    author: {
+      org: {
+        type: "orgpharmacy",
+        name: "Pharmacie - code 200",
+        nihdi: "80000551",
+      },
+    },
+    patient: {
+      familyname: "Doe",
+      firstname: "John",
+      ssin: "12345678901",
+    },
+    //date: "2024-02-01",
+    deliveries: [
+      {
+        deliveredAmount: 2,
+        deliveredMode: "dispensedWithoutPrescription",
+        drugs: [
+          // Returned medication (-1)
+          {
+            quantity: {
+              amount: -1
+            },
+            drugType: "medicinalproduct",
+            intendedcd: "1080233",
+            intendedname: "Iso-Betadine Dermicum 10 % cut. opl. 50 x 10 ml UD",
+            instructionForPatient: "productCode",
+          },
+          // Long medication name returned
+          {
+            quantity: {
+              amount: -1
+            },
+            drugType: "medicinalproduct",
+            intendedcd: "3639408",
+            intendedname: "Efavirenz/Emtricitabine/Tenofovir Disoproxil Mylan 600 mg - 200 mg - 245 mg filmomh. tabl. 90 (3 x 30)",
+            instructionForPatient: "productCode",
+          },
+        ],
+      },
+    ],
+  };
+}
